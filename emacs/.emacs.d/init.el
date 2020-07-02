cbf4e6aeda5b735476ddfb934fc71b0bfb1810bc
Raptime <raptime@users.noreply.github.com>
2020-07-02 16:03:14 -0400

    new emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,47 +1,29 @@
 ;; init.el --- Emacs configuration
 
-;; INSTALL PACKAGES
-;; --------------------------------------
-
-(require 'package)
-
-;;(add-to-list 'package-archives
-;;             '("melpa" . "http://melpa.org/packages/") t)
-
-;;(package-initialize)
-;;(when (not package-archive-contents)
-;;  (package-refresh-contents))
-
-;;(defvar myPackages
-;;  '(better-defaults     ;; better defaults
-;;    solarized-theme))   ;; save my eyes theme
-
-;;(mapc #'(lambda (package)
-;;          (unless (package-installed-p package)
-;;            (package-install package)))
-;;      myPackages)
-
 ;; BASIC CUSTOMIZATION
 ;; --------------------------------------
-
-(setq inhibit-startup-message t) ;; hide the startup message
-;;(load-theme 'solarized-dark t)   ;; load solarized theme
-(custom-set-faces (if (not window-system) '(default ((t (:background "nil"))))))
-(global-linum-mode t)            ;; enable line numbers globally
-(column-number-mode t)           ;; Show column number as well as line
-
+(setq inhibit-startup-message t)     ;; hide the startup message
 (setq default-major-mode 'text-mode) ;; text mode for new buffers
+(setq visible-bell t)                ;; disable the bell
+(setq linum-format "%d ")            ;; add a space after line numbers
+
 (defalias 'yes-or-no-p 'y-or-n-p)    ;; replace yes or no with y or n
-(setq ring-bell-function 'ignore)    ;; disable the bell
 
-(setq linum-format "%d ")	;; add a space after line numbers
-(menu-bar-mode -1)		;; disable the menu bar
-(if (functionp 'tool-bar-mode) (tool-bar-mode -1))		;; disable the tool bar
+(save-place-mode 1)
+(show-paren-mode 1)
 
-(set-face-attribute 'default nil :font "Liberation Mono-10") ;; set default font
+(unless (eq window-system 'ns)
+  (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(when (fboundp 'horizontal-scroll-bar-mode)
+  (horizontal-scroll-bar-mode -1))
 
-(setq backup-directory-alist `(("." . "~/.saves"))) ;; move backups out of the current folder
-
+(unless backup-directory-alist
+  (setq backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                                 "backups")))))
 
 ;; FUNCTIONS
 ;; --------------------------------------
@@ -54,8 +36,6 @@
 
 ;; KEY BINDINGS
 ;; --------------------------------------
-
 (global-set-key [f6]            'next-multiframe-window)
 (global-set-key [f7]            'next-buffer)
 (global-set-key [f12]           'iwb)
-

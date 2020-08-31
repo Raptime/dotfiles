71fa9355ceb8cd94ddb2d595243a2fe65edbab14
Raptime <raptime@users.noreply.github.com>
2020-08-30 23:10:02 -0400

    emac config cleanup

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -58,35 +58,26 @@
 
 ;; BASIC CUSTOMIZATION
 ;; --------------------------------------
-(setq inhibit-startup-message t)     ;; hide the startup message
-(setq default-major-mode 'text-mode) ;; text mode for new buffers
-(setq visible-bell t)                ;; disable the bell
-(setq mouse-wheel-scroll-amount
-      '(1 ((shift) . 1) ((control) . nil)))
-(setq mouse-wheel-progressive-speed nil)
+(setq inhibit-startup-message t)        ; hide the startup message
+(setq default-major-mode 'text-mode)    ; text mode for new buffers
+(setq visible-bell t)                   ; disable the bell
+(setq confirm-kill-processes nil)       ; kill processes on exit
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                               "backups"))))
+					; move backup folder
 
-(defalias 'yes-or-no-p 'y-or-n-p)    ;; replace yes or no with y or n
-(setq confirm-kill-processes nil)
+(defalias 'yes-or-no-p 'y-or-n-p)       ; replace yes or no with y or n
 
-(save-place-mode 1)
-(show-paren-mode 1)
+(save-place-mode 1)                     ; remember place in file
+(show-paren-mode 1)                     ; show matching parenthesis
+(delete-selection-mode 1)               ; allow selection replacement
 
-;; Bars
-(unless (eq window-system 'ns)
-  (menu-bar-mode -1))
-(when (fboundp 'tool-bar-mode)
-  (tool-bar-mode -1))
-(when (fboundp 'scroll-bar-mode)
-  (scroll-bar-mode -1))
-(when (fboundp 'horizontal-scroll-bar-mode)
-  (horizontal-scroll-bar-mode -1))
+(menu-bar-mode -1)                      ; hide menu bar
+(tool-bar-mode -1)                      ; hide tool bar
+(scroll-bar-mode -1)                    ; hide scroll bar
+(horizontal-scroll-bar-mode -1)         ; hide horizontal scroll bar
 
-;; Backups
-(unless backup-directory-alist
-  (setq backup-directory-alist `(("." . ,(concat user-emacs-directory
-                                                 "backups")))))
-
-;; Emoji: 😄, 🤦, 🏴󠁧󠁢󠁳󠁣󠁴󠁿
+;; Emoji fonts: 😄, 🤦, 🏴󠁧󠁢󠁳󠁣󠁴󠁿
 (set-fontset-font t 'symbol "Noto Color Emoji")
 (set-fontset-font t 'symbol "Apple Color Emoji" nil 'append)
 (set-fontset-font t 'symbol "Segoe UI Emoji" nil 'append)
